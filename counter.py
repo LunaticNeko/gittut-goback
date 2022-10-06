8675507bb2e1ea932fbbebb6f17bf58ae5e50940
Chawanat Nakasan <chawanat.n@gmail.com>
2022-10-07 02:23:54 +0700

    change counter behavior

--- counter.py
+++ counter.py
@@ -13,15 +13,7 @@ class Counter:
     def get_value(self):
         return self.value
 
-# You found the hidden message.
-# Please write your student ID in the following line of code and send the
-# result to Google Classroom for evaluation.
-
-student_id = b"65XXXXXXXX" # keep the quotes
-
 if __name__ == "__main__":
-    from hashlib import sha256
-    print(sha256(student_id + b"asdfghjkl").hexdigest())
     c = Counter(100)
     c.up(100)
     c.down(200)
